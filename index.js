436816fe3926862eb9083490089d4236a4618bc5
pitust <stelmaszek.piotrpilot@gmail.com>
2020-05-02 12:53:09 +0100

    Fixed a bug

--- index.js
+++ index.js
@@ -8,49 +8,53 @@ client.on('ready', () => {
 });
 
 client.on('message', async msg => {
-    if (msg.author.bot) return;
-    if (msg.content === '!csvdecode') {
-        let [a, chk] = msg.attachments.array();
-        if (chk || !a) {
-            msg.reply('Add only one file to decode!');
-            return;
+    try {
+        if (msg.author.bot) return;
+        if (msg.content === '!csvdecode') {
+            let [a, chk] = msg.attachments.array();
+            if (chk || !a) {
+                msg.reply('Add only one file to decode!');
+                return;
+            }
+            let m = await msg.reply('Downloading attachment...');
+            /**
+             * @type {fetch.Response}
+             */
+            let res = await fetch(a.url);
+            let buf = await res.buffer();
+            await m.edit('Decompressing...');
+            let data = ScCompression.decompress(buf);
+            let em = new Discord.MessageEmbed();
+            em.setAuthor('Made by pitust#8711');
+            em.setColor('RED');
+            em.setTitle('Your file has been decompressed!');
+            let at = new Discord.MessageAttachment(data, a.name);
+            em.attachFiles([at]);
+            await m.channel.send(em)
         }
-        let m = await msg.reply('Downloading attachment...');
-        /**
-         * @type {fetch.Response}
-         */
-        let res = await fetch(a.url);
-        let buf = await res.buffer();
-        await m.edit('Decompressing...');
-        let data = ScCompression.decompress(buf);
-        let em = new Discord.MessageEmbed();
-        em.setAuthor('Made by pitust#8711');
-        em.setColor('RED');
-        em.setTitle('Your file has been decompressed!');
-        let at = new Discord.MessageAttachment(data, a.name);
-        em.attachFiles([at]);
-        await m.channel.send(em)
-    }
-    if (msg.content === '!info') {
-        msg.reply('The <@706058387942539285> is a bot which can decompress CSV files');
-    }
-    if (msg.content === '!help') {
-        const embed = {
-            "color": 1145312,
-            "fields": [
-                {
-                    "name": "**More info**",
-                    "value": "`!info`",
-                    "inline": true
-                },
-                {
-                    "name": "**Decode CSV (attached)**",
-                    "value": "`!csvdecode`",
-                    "inline": true
-                }
-            ]
-        };
-        msg.reply(new Discord.MessageEmbed(embed));
+        if (msg.content === '!info') {
+            msg.reply('The <@706058387942539285> is a bot which can decompress CSV files');
+        }
+        if (msg.content === '!help') {
+            const embed = {
+                "color": 1145312,
+                "fields": [
+                    {
+                        "name": "**More info**",
+                        "value": "`!info`",
+                        "inline": true
+                    },
+                    {
+                        "name": "**Decode CSV (attached)**",
+                        "value": "`!csvdecode`",
+                        "inline": true
+                    }
+                ]
+            };
+            msg.reply(new Discord.MessageEmbed(embed));
+        }
+    } catch (e) {
+        await msg.reply('Error (DM pitust#8711 with the error)\n' + e.stack);
     }
 });
 
